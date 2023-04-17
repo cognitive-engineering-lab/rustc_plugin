9e0dfaae062c34bf3bb25c48679c8fd70fe615cd
Will Crichton <wcrichto@cs.stanford.edu>
2023-04-16 19:55:23 -0700

    Add clippy to rust-toolchain.toml

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 set -e
 
 CHANNEL=$(awk -F'"' '/^channel = / {print $2}' rust-toolchain.toml)
-VERSION=$(awk -F'"' '/^version = / {print $2}' Cargo.toml)
+VERSION=$(awk -F'"' '/^version = / {print $2}' crates/rustc-utils/Cargo.toml)
 TAG="${CHANNEL}-v${VERSION}"
 git tag $TAG
 echo "Created tag: $TAG"
