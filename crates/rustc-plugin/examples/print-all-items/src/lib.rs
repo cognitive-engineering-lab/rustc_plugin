b0ac77a78ca0b9096ed55a29de24df904ec44e0e
Will Crichton <wcrichto@cs.stanford.edu>
2023-04-18 20:25:38 -0700

    Add commentary to the example

--- crates/rustc-plugin/examples/print-all-items/src/lib.rs
+++ crates/rustc-plugin/examples/print-all-items/src/lib.rs
@@ -1,3 +1,5 @@
+//! A Rustc plugin that prints out the name of all items in a crate.
+
 #![feature(rustc_private)]
 
 extern crate rustc_driver;
@@ -11,8 +13,12 @@ use rustc_middle::ty::TyCtxt;
 use rustc_plugin::{CrateFilter, RustcPlugin, RustcPluginArgs, Utf8Path};
 use serde::{Deserialize, Serialize};
 
+// This struct is the plugin provided to the rustc-plugin framework,
+// and it must be exported for use by the CLI/driver binaries.
 pub struct PrintAllItemsPlugin;
 
+// To parse CLI arguments, we use Clap for this example. But that
+// detail is up to you.
 #[derive(Parser, Serialize, Deserialize)]
 pub struct PrintAllItemsPluginArgs {
   #[arg(short, long)]
@@ -30,12 +36,17 @@ impl RustcPlugin for PrintAllItemsPlugin {
     "print-all-items-driver".into()
   }
 
+  // In the CLI, we ask Clap to parse arguments and also specify a CrateFilter.
+  // If one of the CLI arguments was a specific file to analyze, then you
+  // could provide a different filter.
   fn args(&self, _target_dir: &Utf8Path) -> RustcPluginArgs<Self::Args> {
     let args = PrintAllItemsPluginArgs::parse_from(env::args().skip(1));
     let filter = CrateFilter::AllCrates;
     RustcPluginArgs { args, filter }
   }
 
+  // In the driver, we use the Rustc API to start a compiler session
+  // for the arguments given to us by rustc-plugin.
   fn run(
     self,
     compiler_args: Vec<String>,
@@ -52,20 +63,32 @@ struct PrintAllItemsCallbacks {
 }
 
 impl rustc_driver::Callbacks for PrintAllItemsCallbacks {
+  // At the top-level, the Rustc API uses an event-based interface for
+  // accessing the compiler at different stages of compilation. In this callback,
+  // all the type-checking has completed.
   fn after_analysis<'tcx>(
     &mut self,
     _compiler: &rustc_interface::interface::Compiler,
     queries: &'tcx rustc_interface::Queries<'tcx>,
   ) -> rustc_driver::Compilation {
+    // We extract a key data structure, the `TyCtxt`, which is all we need
+    // for our simple task of printing out item names.
     queries
       .global_ctxt()
       .unwrap()
       .enter(|tcx| print_all_items(tcx, &self.args));
 
+    // Note that you should generally allow compilation to continue. If
+    // your plugin is being invoked on a dependency, then you need to ensure
+    // the dependency is type-checked (its .rmeta file is emitted into target/)
+    // so that its dependents can read the compiler outputs.
     rustc_driver::Compilation::Continue
   }
 }
 
+// The core of our analysis. It doesn't do much, just access some methods on the `TyCtxt`.
+// I recommend reading the Rustc Development Guide to better understand which compiler APIs
+// are relevant to whatever task you have.
 fn print_all_items(tcx: TyCtxt, args: &PrintAllItemsPluginArgs) {
   let hir = tcx.hir();
   for item_id in hir.items() {
