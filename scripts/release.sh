fc77e2f9e688faf206e4414fc8d7d5fd8d7b3346
Will Crichton <wcrichto@cs.stanford.edu>
2023-05-22 16:07:31 -0700

    Fix release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-CHANNEL=$(awk -F'"' '/^channel = / {print $2}' rust-toolchain.toml)
 VERSION=$(awk -F'"' '/^version = / {print $2}' crates/rustc-utils/Cargo.toml)
-TAG="v${VERSION}-${CHANNEL}"
+TAG="v${VERSION}"
 git tag $TAG
 echo "Created tag: $TAG"
 echo "Don't forget to update the docs link in the README!"
