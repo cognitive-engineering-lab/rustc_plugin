5976047c2b7f1c467a09d764599c37bb0004345a
willcrichton <wcrichto@cs.stanford.edu>
2023-09-12 21:50:57 +0000

    Deploying to gh-pages from @ cognitive-engineering-lab/rustc_plugin@43e787a2e625334d0f0ac99f214f5a6e93338fec 🚀

---
+++ v0.7.1-nightly-2023-08-25/src-files.js
@@ -0,0 +1,24 @@
+var srcIndex = JSON.parse('{\
+"anyhow":["",[],["backtrace.rs","chain.rs","context.rs","ensure.rs","error.rs","fmt.rs","kind.rs","lib.rs","macros.rs","ptr.rs","wrapper.rs"]],\
+"camino":["",[],["lib.rs","serde_impls.rs"]],\
+"cargo_metadata":["",[],["dependency.rs","diagnostic.rs","errors.rs","lib.rs","messages.rs"]],\
+"cargo_platform":["",[],["cfg.rs","error.rs","lib.rs"]],\
+"cfg_if":["",[],["lib.rs"]],\
+"intervaltree":["",[],["lib.rs"]],\
+"itoa":["",[],["lib.rs","udiv128.rs"]],\
+"log":["",[],["__private_api.rs","lib.rs","macros.rs"]],\
+"proc_macro2":["",[],["detection.rs","extra.rs","fallback.rs","lib.rs","marker.rs","parse.rs","rcvec.rs","wrapper.rs"]],\
+"quote":["",[],["ext.rs","format.rs","ident_fragment.rs","lib.rs","runtime.rs","spanned.rs","to_tokens.rs"]],\
+"rustc_plugin":["",[],["cli.rs","driver.rs","lib.rs","plugin.rs"]],\
+"rustc_tools_util":["",[],["lib.rs"]],\
+"rustc_utils":["",[["hir",[],["mod.rs","ty.rs"]],["mir",[],["adt_def.rs","body.rs","borrowck_facts.rs","control_dependencies.rs","location_or_arg.rs","mod.rs","mutability.rs","operand.rs","place.rs","places_conflict.rs"]],["source_map",[["spanner",[],["hir_span.rs","mir_span.rs","mod.rs","span_tree.rs"]]],["filename.rs","find_bodies.rs","mod.rs","range.rs","span.rs"]]],["cache.rs","lib.rs","timer.rs"]],\
+"ryu":["",[["buffer",[],["mod.rs"]],["pretty",[],["exponent.rs","mantissa.rs","mod.rs"]]],["common.rs","d2s.rs","d2s_full_table.rs","d2s_intrinsics.rs","digit_table.rs","f2s.rs","f2s_intrinsics.rs","lib.rs"]],\
+"semver":["",[],["backport.rs","display.rs","error.rs","eval.rs","identifier.rs","impls.rs","lib.rs","parse.rs","serde.rs"]],\
+"serde":["",[["de",[],["format.rs","ignored_any.rs","impls.rs","mod.rs","seed.rs","size_hint.rs","value.rs"]],["private",[],["de.rs","doc.rs","mod.rs","ser.rs"]],["ser",[],["fmt.rs","impls.rs","impossible.rs","mod.rs"]]],["integer128.rs","lib.rs","macros.rs"]],\
+"serde_derive":["",[["internals",[],["ast.rs","attr.rs","case.rs","check.rs","ctxt.rs","mod.rs","receiver.rs","respan.rs","symbol.rs"]]],["bound.rs","de.rs","dummy.rs","fragment.rs","lib.rs","pretend.rs","ser.rs","this.rs"]],\
+"serde_json":["",[["features_check",[],["mod.rs"]],["io",[],["mod.rs"]],["value",[],["de.rs","from.rs","index.rs","mod.rs","partial_eq.rs","ser.rs"]]],["de.rs","error.rs","iter.rs","lib.rs","macros.rs","map.rs","number.rs","read.rs","ser.rs"]],\
+"smallvec":["",[],["lib.rs"]],\
+"syn":["",[["gen",[],["clone.rs"]]],["attr.rs","bigint.rs","buffer.rs","custom_keyword.rs","custom_punctuation.rs","data.rs","derive.rs","discouraged.rs","drops.rs","error.rs","export.rs","expr.rs","ext.rs","gen_helper.rs","generics.rs","group.rs","ident.rs","lib.rs","lifetime.rs","lit.rs","lookahead.rs","mac.rs","macros.rs","meta.rs","op.rs","parse.rs","parse_macro_input.rs","parse_quote.rs","path.rs","print.rs","punctuated.rs","restriction.rs","sealed.rs","span.rs","spanned.rs","thread.rs","token.rs","ty.rs","verbatim.rs"]],\
+"unicode_ident":["",[],["lib.rs","tables.rs"]]\
+}');
+createSrcSidebar();
