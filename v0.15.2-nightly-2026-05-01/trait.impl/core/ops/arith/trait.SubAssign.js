d3dbad145caf417bbaa66e8146246b994dab4c5e
willcrichton <wcrichto@cs.stanford.edu>
2026-05-03 23:52:08 +0000

    Deploying to gh-pages from @ cognitive-engineering-lab/rustc_plugin@fdfd14e3802e035e5d0b70685541faec7e153518 🚀

---
+++ v0.15.2-nightly-2026-05-01/trait.impl/core/ops/arith/trait.SubAssign.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["rustc_utils",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.SubAssign.html\" title=\"trait core::ops::arith::SubAssign\">SubAssign</a> for <a class=\"struct\" href=\"rustc_utils/mir/location_or_arg/index/struct.LocationOrArgIndex.html\" title=\"struct rustc_utils::mir::location_or_arg::index::LocationOrArgIndex\">LocationOrArgIndex</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.SubAssign.html\" title=\"trait core::ops::arith::SubAssign\">SubAssign</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>&gt; for <a class=\"struct\" href=\"rustc_utils/mir/location_or_arg/index/struct.LocationOrArgIndex.html\" title=\"struct rustc_utils::mir::location_or_arg::index::LocationOrArgIndex\">LocationOrArgIndex</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[865]}
---
+++ v0.15.2-nightly-2026-05-01/trait.impl/rustc_index/idx/trait.Idx.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["rustc_utils",[["impl Idx for <a class=\"struct\" href=\"rustc_utils/mir/location_or_arg/index/struct.LocationOrArgIndex.html\" title=\"struct rustc_utils::mir::location_or_arg::index::LocationOrArgIndex\">LocationOrArgIndex</a>",0],["impl Idx for <a class=\"struct\" href=\"rustc_utils/source_map/filename/struct.FilenameIndex.html\" title=\"struct rustc_utils::source_map::filename::FilenameIndex\">FilenameIndex</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[425]}
